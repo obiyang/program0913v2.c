70a8096e487692b10be084bcee382a1cb243f60c
obiyang <binyang1994@126.com>
2023-09-13 17:33:35 -0700

    Initial Commit

---
+++ program0913v2.c/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  program0913v2.c
+//
+//  Created by Bin Yang on 9/13/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
